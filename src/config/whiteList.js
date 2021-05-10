a9bfafee71fedeff9d1e88a94f040720800595fa
martin <joghur@gmail.com>
2021-05-10 12:49:25 +0200

    refactoring dev setup

--- src/config/whiteList.js
+++ src/config/whiteList.js
@@ -1,11 +1,6 @@
-const devPages = [
-	'http://localhost:3000',
-	'http://localhost:4000',
-	'https://sandersolutions.dk',
-	undefined,
-]
+import { devPages } from './devPages';
 
 export const whitelist = [
-	(process.env.NODE_ENV!=="production" && ...devPages),
+	...[process.env.NODE_ENV !== 'production' && devPages],
 	process.env.CORS_ORIGIN,
 ];
